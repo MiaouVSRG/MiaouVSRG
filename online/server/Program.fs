dabb94aba2423bc50141cd9b8588afda6157cfe3
KyunMeow <bodin.bastien49@gmail.com>
2025-07-13 15:36:12 +0200

    Fix bot issue for server

--- online/server/Program.fs
+++ online/server/Program.fs
@@ -50,8 +50,8 @@ try
     Server.start ()
     Logging.Info "Launching api on port %i ..." HTTPS_PORT
     API.Server.start ()
-    Logging.Info "Launching discord bot ..."
-    Bot.start ()
+    // Logging.Info "Launching discord bot ..."
+    // Bot.start ()
 
 #if DEBUG
     Console.ReadLine() |> ignore
